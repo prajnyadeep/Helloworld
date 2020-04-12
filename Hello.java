417e658eb4b2eb84416694e58f0d794ba45e712a
Prajnyadeep <prajnyadeep.subhashis@gmail.com>
2020-04-12 23:38:29 +0530

    commit second

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	
 	public static void main(String[] args){
 		for(int i=0;i<10;i++){
-			System.out.println("Hello world..."+i);
+			System.out.println("Hello my world..."+i);
 		}
 	}
 }
